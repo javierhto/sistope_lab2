429d044947d486edd3542587480b6bb28ea57888
usuario <>
2019-06-10 20:04:57 -0400

    intentando implementar monitores

--- funciones.c
+++ funciones.c
@@ -214,3 +214,125 @@ void writeFile(double* informacionHijos, char* nombreArchivo, int numDisco){
       //Liberamos la memoria del resto.
       free(childs);
     */
+
+   //Función que lee una línea de un archivo de texto desde un archivo
+//Entrada: Puntero al archivo del cuál se va a leer
+//Salida: Cadena de char
+char* readLine(FILE* file){
+  int i = 0;
+  //Asignación de memoria para las cadenas
+  char* line = (char*)malloc(sizeof(char)*128);
+  char* ch = (char*)malloc(sizeof(char)*64);
+  
+  int read;
+  while((read = fread(ch, sizeof(char), 1, file)) == 1){
+    if(ch[0] == 10){
+      line[i] = '\0';
+      i++;
+      break;
+    }
+    else{
+      line[i] = ch[0];
+      i++;
+    }
+  }
+  if(line[i] == 10){
+    line[i] = '\0';
+  }
+  if(read == 0){
+    line[0] = '\0';
+  }
+  return line;
+}
+
+//Funnción que retorna el int correspondiente al disco que le corresponde la coordenada de entrada.
+//Entrada: Coordenadas u,v, cantidad de discos y ancho de los discos
+//Salida: Disco de destino
+int checkDestination(double coordV, double coordU, int discWidth, int discCant){
+    double distanceUV;
+    double maxRadius = discWidth*discCant;
+    distanceUV = sqrt(pow(coordV,2)+ pow(coordU,2)); //Se calcula la distancia con la fórmula entregada
+    int disc = (distanceUV/discWidth);
+    if(distanceUV > maxRadius){ //Esto hace que el último disco albergue todas las visibilidades que queden fuera del espectro máximo
+       return discCant - 1;
+    }
+    else{
+       return disc;
+    }
+}
+
+//Función que inicializa un arreglo de char
+//Entrada: Arreglo y largo del arreglo
+//Salida: Nada, solo referencia
+void inicializarCharArray(char* array, int largo){
+    int i;
+    for(i = 0; i < largo; i++){
+      array[i] = 0;
+    }
+}
+
+//Función que lee la visibilidad desde un cadena de datos
+//Entrada: Cadena de datos, ancho del disco, cantidad de discos
+//Salida: Visibilidad como entero
+int obtenerVisibilidadRecibida(char* visibilidad, int discWidth, int discCant){
+    int i; //DECLARACION DE i PARA EL CICLO FOR.
+    int j = 0; //DECLARACION DE j PARA POSICIONAR DOUBLE EN ARREGLO.
+    int k = 0; //DECLARACION DE k PARA POSICIONAR CHAR EN ARREGLO.
+    int len = strlen(visibilidad); //OBTENGO EL LARGO DEL STRING RECIBIDO.
+    int arrayLength = 32;
+    double* data = (double*)malloc(len*sizeof(double));
+    char* temp = (char*)malloc(arrayLength*sizeof(char));
+    inicializarCharArray(temp, arrayLength); //INICIALIZO EL ARREGLO PARA QUE NO TENGA BASURA.
+    for(i = 0; i <= len; i++){
+      if(i == len){ //SI LOS LARGOS SON IGUALES, SIGNIFICA QUE TERMINE DE PROCESAR LA INFORMACION.
+        data[j] = atof(temp);
+        free(temp);
+      }
+      else if(visibilidad[i] == 44){ //SI EN LA POSICION HAY UNA ',', SIGNIFICA QUE TERMINE DE LEER UN DATO.
+        data[j] = atof(temp);
+        j++;
+        k = 0;
+        free(temp); //LIBERO LA MEMORIA PARA EVITAR LEAKS.
+        temp = (char*)malloc(32*sizeof(char)); //LE ASIGNO ESPACIO EN LA RAM NUEVAMENTE.
+        inicializarCharArray(temp, arrayLength); //LE VUELVO A ASIGNAR CEROS PARA QUE NO HAYA BASURA.
+      }
+      else{
+        temp[k] = visibilidad[i];
+        k++;
+      }
+    }
+    return checkDestination(data[0], data[1], discWidth, discCant); //BUSCO EL DISCO AL QUE PERTENECE LA INFO.
+}
+
+//Función que escribe en un archivo los datos procesados por un hijo
+//Entrada: Arreglo de datos de un hijos, nombre del archivo de salida, número de disco actual
+//Salida: Nada
+void writeFile(double* informacionHijos, char* nombreArchivo, int numDisco){
+  FILE *fp;
+  int i;
+  char** str = (char**)malloc(4*sizeof(char*));
+  for(i = 0; i < 4 ; i++){
+    str[i] = (char*)malloc(20*sizeof(char));
+  }
+  str[0] = "Media real: ";
+  str[1] = "Media imaginaria: ";
+  str[2] = "Potencia: ";
+  str[3] = "Ruido total: ";
+  char numArr[512];
+  sprintf(numArr, "%d", numDisco);
+  char disco[] = "Disco ";
+  char endDisc[] = ":\r\n";
+  char endLine[] = "\r\n";
+  fp = fopen(nombreArchivo, "a+b");
+  fwrite(disco, sizeof(char) , strlen(disco), fp);
+  fwrite(numArr, sizeof(char) , strlen(numArr), fp);
+  fwrite(endDisc, sizeof(char), strlen(endDisc), fp);
+  for(i = 0; i < 4; i++){
+    fwrite(str[i], sizeof(char), strlen(str[i]), fp);
+    sprintf(numArr, "%lf", informacionHijos[i]);
+    fwrite(numArr, sizeof(char), strlen(numArr), fp);
+    fwrite(endLine, sizeof(char), strlen(endLine), fp);
+  }
+  fwrite(endLine, sizeof(char), strlen(endLine), fp);
+  fclose(fp);
+}
